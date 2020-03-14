fb5321c263d5e468757d746e7cacc780a770cf12
PedroFischerDeveloper <pedro.fischer@fatec.sp.gov.br>
2020-03-13 21:30:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fatec-si-ng-loja-4zw6kw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/fatec-si-ng-loja-4zw6kw)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
+import {CartService} from '../cart.service.ts';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements 
+OnInit {
+  product;
+
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Novo produto Adicionado!');
+  }
+
+
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
+    });
+  }
+
+}
